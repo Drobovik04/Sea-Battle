9474cb8ab267c0e1e72f88724530a08030590157
Victor Voykel <misle92@mail.ru>
2023-04-03 16:59:57 +0300

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] Tilemap tileMap;
     GameObject current;
     Transform currentPos;
+    SpriteRenderer rendererPrefab;
+    [SerializeField]Tile shipTile;
+    [SerializeField]int shipLength;
     Camera main;
     // Start is called before the first frame update
     void Awake()
@@ -22,14 +25,13 @@ public class GameManager : MonoBehaviour
         if (current!=null)
         {
             Vector3 vec = main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, main.nearClipPlane);
-            vec = tileMap.WorldToCell(vec) + tileMap.tileAnchor;
-            
+            vec = tileMap.WorldToCell(vec);
             currentPos.position = vec;
             if (Input.GetMouseButton(0))
             {
                 if (Check())
                 {
-                    //tileMap.SetTiles();
+                    PlaceShip();
                 }
             }
         }
@@ -39,18 +41,25 @@ public class GameManager : MonoBehaviour
         Vector3 worldCooridnates= main.ScreenToWorldPoint(Input.mousePosition);
         current = Instantiate(obj, worldCooridnates, Quaternion.identity);
         currentPos = current.GetComponent<Transform>();
+        shipLength = (int)obj.GetComponent<SpriteRenderer>().size.x;
+        Debug.Log(shipLength);
     }
     private bool Check()
     {
         for (int i=0; i<current.GetComponent<SpriteRenderer>().size.x;i++)
         {
-            Vector3Int coor = new Vector3Int((int)currentPos.position.x, (int)currentPos.position.y, (int)currentPos.position.z) + new Vector3Int(1+i,0,0);
-            if (tileMap.GetTile(coor)!=null) return false;
+            Vector3Int coor = new Vector3Int((int)currentPos.position.x, (int)currentPos.position.y, (int)currentPos.position.z) + new Vector3Int(i-shipLength/2,0,0);
+            if (tileMap.GetTile(tileMap.WorldToCell(coor))!=null) return false;
         }
         return true; 
     }
     public void PlaceShip()
     {
-
+        Vector3Int pos = new Vector3Int((int)currentPos.position.x, (int)currentPos.position.y, (int)currentPos.position.z);
+        pos = tileMap.WorldToCell(pos);
+        for (int i = 0; i < shipLength; i++)
+        {
+            tileMap.SetTile(pos + new Vector3Int(i-(int)shipLength / 2, 0,0), shipTile);
+        }
     }
 }
